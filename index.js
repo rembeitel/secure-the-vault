5b19b78236309665db13cf377413caefdddd2ee8
Rem Beitel <rbeitel4@gmail.com>
2025-09-07 23:55:50 -0500

    completed js homework

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+var code1 = 7 + 3;
+var code2 = 410 / 5 - 42;
+var code3 = 1271 % 112;
+let message = "The vault has been secured. The combination is:";
+var codeA = "" + code1 + -code2 + -code3;
+var codeB = `` + code1 + -code2 + -code3;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
